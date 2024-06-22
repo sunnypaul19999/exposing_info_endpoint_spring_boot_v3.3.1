06c8e14b11412ae14c9556067bf315196d272fd1
sunnypaul19999 <sunnypaul19999@gmail.com>
2024-06-22 13:01:24 +0530

    begin

---
+++ src/main/java/com/ra/myapp/MyApp.java
@@ -0,0 +1,13 @@
+package com.ra.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApp.class, args);
+	}
+
+}
